8abe2862e3c8456b9a254861d35a2bd987ab00c4
Akiboy0310 <ahmedmohamed0310@outlook.at>
2023-02-25 13:26:16 +0100

    addded CNN model

--- feature_extraction.py
+++ feature_extraction.py
@@ -38,11 +38,27 @@ def concateDf(df1,df2,df3,df4):
     df4 = addLable(df4, title_light_dim)
     df = pd.concat([df1,df2])
     return df
+def concateDf_num(df1,df2,df3,df4):
+    #add nummeric labels to df for Neural Network then concatenate them
+    df1 = addLable(df1, 1)
+    df2 = addLable(df2, 2)
+    df3 = addLable(df3, 3)
+    df4 = addLable(df4, 4)
+    df = pd.concat([df1,df2])
+    return df
 def dwt_concat(df1,df2,df3,df4):
     dwt_df1 = wavelet_transform(df1)
     dwt_df2 = wavelet_transform(df2)
     dwt_df3 = wavelet_transform(df3)
     dwt_df4 = wavelet_transform(df4)
+    # Find the length of the smallest data frame
+    min_len = min(len(df) for df in [dwt_df1, dwt_df2, dwt_df3,dwt_df4])
+
+    # Select the first n rows of each data frame
+    dwt_df1 = dwt_df1.iloc[:min_len]
+    dwt_df2 = dwt_df2.iloc[:min_len]
+    dwt_df3 = dwt_df3.iloc[:min_len]
+    dwt_df4 = dwt_df4.iloc[:min_len]
     concat_dwt_dfs = concateDf(dwt_df1, dwt_df2, dwt_df3, dwt_df4)
     #concat_dwt_dfs.to_csv("After_DWT_and_Concat.csv",sep=";")
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import *
 from classification_LDA import *
+from Classification_CNN import *
 from feature_extraction import *
 from process_data import *
 
@@ -13,7 +14,9 @@ def main():
     df_1_features= dwt_concat(df1_l1, df1_l2, df1_l3, df1_l4)
     print(f"Hallo YOU \n: {df_1_features}")
     train_test_valid_LDA(df_1_features)
-
+    df_1_concat=concateDf_num(df1_l1, df1_l2, df1_l3, df1_l4)
+    print(df_1_concat)
+    train_test_valid_CNN(df_1_concat)
     #using the second Dataset
     df2_l1, df2_l2, df2_l3, df2_l4 = process(df_ds2)
     df_2_features= dwt_concat(df2_l1, df2_l2, df2_l3, df2_l4)
