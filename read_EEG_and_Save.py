3b9af6cb832f1d67b1db472523a21064f5579df5
Akiboy0310 <ahmedmohamed0310@outlook.at>
2023-03-26 10:07:59 +0200

    Made Sript run 30 s

--- read_EEG_and_Save.py
+++ read_EEG_and_Save.py
@@ -1,17 +1,22 @@
+import sys
+from pyOpenBCI import OpenBCICyton
+import csv
+import os.path
+import time
 import utils
 
 start_time = time.time()
-time_limit = 15  # 15 seconds
+time_limit = 30  # 30 seconds
 
-label='light_off'
+label='light_onn'
 # define a callback function to print the raw data
 def print_raw(sample):
     # create a CSV file to write the data to, or open the file in append mode if it already exists
-    if os.path.exists('eeg_data_new.csv'):
-        csvfile = open('eeg_data_new.csv', 'a')
+    if os.path.exists('test.csv'):
+        csvfile = open('test.csv', 'a')
         writer = csv.writer(csvfile)
     else:
-        csvfile = open('eeg_data_new.csv', 'w')
+        csvfile = open('test.csv', 'w')
         writer = csv.writer(csvfile)
         # write the column headers for the 8 channels and the timestamp
         writer.writerow(["timestamp", "channel1", "channel2", "channel3", "channel4", "channel5", "channel6", "channel7", "channel8", "label"])
@@ -24,17 +29,23 @@ def print_raw(sample):
     csvfile.close()
     print(sample.channels_data)
 
+    # check if the duration has elapsed
+    if time.time() - start_time >= time_limit:
+        # stop the stream and exit the program
+        board.stop_stream()
+        sys.exit()
 # create an OpenBCI object and connect to the Cyton board
 board = OpenBCICyton(port='COM4', daisy=False)
 
 # set the sample rate for the board
 #board.set_sample_rate(250)
-# start the stream and pass the callback function
+
+# run the stream and pass samples to callback function
 board.start_stream(print_raw)
 
-# collect data 15 seconds long
-while True:
-    time.sleep(1)
-    # Check if the time limit has been reached
-    if time.time() - start_time > time_limit:
-        break
+# collect data 30 seconds long
+#while True:
+
+
+    # run the stream and pass samples to callback function
+   # board.start_stream(print_raw)
